54ca43228111aa199e69e9ba1c8b1e1a4dce18a8
RSDNS\AnkurP <ankurp@rssoftware.co.in>
2018-06-12 16:09:41 +0530

    Added BC

--- src/main/java/com/test/automation/selenium/businesscomponents/Login.java
+++ src/main/java/com/test/automation/selenium/businesscomponents/Login.java
@@ -1,34 +1,32 @@
 package com.test.automation.selenium.businesscomponents;
-
 import com.test.automation.selenium.framework.Browser;
-import com.test.automation.selenium.businesscomponents.bcEnvironment;
 
 public class Login {
 	public static int intRowNum;
-
-	public static void run(String browserType, int rownum) {
-		intRowNum = rownum;
+	
+	public static void run(String browserType,int rownum){
+		intRowNum= rownum;
 		try {
 			Thread.sleep(4000);
 			Browser.Open(browserType, bcEnvironment.appURL);
 			Browser.InputData("Login_Login", intRowNum);
-		} catch (Exception e) {
+			} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
-
-	public static void goBackToLoginAgain(int rownum) {
-		intRowNum = rownum;
+		
+	public static void goBackToLoginAgain(int rownum){
+		intRowNum= rownum;
 		try {
-			Thread.sleep(4000);
-			Browser.InputData("Login_Login", intRowNum);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				Thread.sleep(4000);
+				Browser.InputData("Login_Login", intRowNum);
+				} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 		}
-
+		
 	}
 
 }
